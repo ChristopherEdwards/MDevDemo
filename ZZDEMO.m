d068adf67ec732958c69e50fcb598e545ec9c504
softhat <softhat@softhat-VirtualBox.(none)>
2013-09-06 16:16:45 -0400

    Add new write username feature

---
+++ ZZDEMO.m
@@ -0,0 +1,8 @@
+ZZDEMO ;KRM/CJE - Example Code Step 1 ; 9/3/13 6:15pm
+ ;; 1.0;;Demo code;;Sept 6, 2013;Build 1
+ ; This is example code used in step 1 of the presentation
+ ; Get username
+GETUNAME
+ ; Writes the name of the current user to the screen
+ W $P(^VA(200,DUZ,0),U,1)
+ Q
