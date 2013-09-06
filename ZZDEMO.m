a3e553f5ea1ab65465b0d5f7b9e0c1d2485c1a89
softhat <softhat@softhat-VirtualBox.(none)>
2013-09-06 16:21:01 -0400

    New requirement

--- ZZDEMO.m
+++ ZZDEMO.m
@@ -1,8 +1,8 @@
-ZZDEMO ;KRM/CJE - Example Code Step 1 ; 9/3/13 6:15pm
+ZZDEMO ;KRM/CJE - Example Code Step 2 ; 9/3/13 6:16pm
  ;; 1.0;;Demo code;;Sept 6, 2013;Build 1
- ; This is example code used in step 1 of the presentation
+ ; This is example code used in step 2 of the presentation
  ; Get username
 GETUNAME
  ; Writes the name of the current user to the screen
- W $P(^VA(200,DUZ,0),U,1)
+ W "User Name: ",$P(^VA(200,DUZ,0),U,1)
  Q
